e827a30b2e9a14fa52dcdd939bcb49db98cd178e
DrL <dushimerosine1@gmail.com>
2023-05-09 10:38:27 +0200

    création de 10 éléments de type string

---
+++ main.py
@@ -0,0 +1,4 @@
+#Création de de 10 éléments de type chaînes de caractères
+if __name__ == '__main__':
+    chaine = ['Table', 'Chaise', 'Carotte', 'Fruits',
+               'Thé', 'Café', 'Veloure', 'Pass', 'Classe', 'Mot']
