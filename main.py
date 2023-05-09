c3610ff292cffb1909743ff94001dbaa8122524f
DrL <dushimerosine1@gmail.com>
2023-05-09 10:50:22 +0200

    affichage des éléments de la liste

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-#Création de de 10 éléments de type chaînes de caractères
 if __name__ == '__main__':
+    # I. Création de de 10 éléments de type chaînes de caractères
+    print("***********I. Création de de 10 éléments de type chaînes de caractères******************")
     chaine = ['Table', 'Chaise', 'Carotte', 'Fruits',
                'Thé', 'Café', 'Veloure', 'Pass', 'Classe', 'Mot']
+    # I.1. Affichage des éléments de la liste 
+    print("----1.Affichage des elements de la liste-------------")
+    print(chaine)
+    
