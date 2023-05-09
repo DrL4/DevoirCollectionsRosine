25a6e05911984d3f691882557d971ce888ef67e6
DrL <dushimerosine1@gmail.com>
2023-05-09 12:38:17 +0200

    retrait accents et creation  liste avec mots en a

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 if __name__ == '__main__':
-    # I. Création de de 10 éléments de type chaînes de caractères
-    print("***********I. Création de de 10 éléments de type chaînes de caractères******************")
+    # I. Création de la liste avec 10 elements de type chaines de caracteres
+    print("***********I. Creation d'une liste de 10 elements de type chaines de caracteres******************\n")
     chaine = ['Table', 'Chaise', 'Carotte', 'Fruits',
-               'Thé', 'Café', 'Veloure', 'Pass', 'Classe', 'Mot']
-    # I.1. Affichage des éléments de la liste 
-    print("----1.Affichage des elements de la liste-------------")
+               'The', 'Cafe', 'Veloure', 'Pass', 'Classe', 'Mot']
+    # I.1. Affichage des elements de la liste 
+    print("----1.Affichage des elements de la liste-------------\n")
     print(chaine)
-    # I.2. Changement du contenu de l'élément numéro 5
-    chaine.insert(5, 'Vélo')
+    # I.2. Changement du contenu de l'element numéro 5
+    print("\n-------------2. Changement du contenu de l'element numero 5-----------------------\n")
+    chaine.insert(5, 'Velo')#insertion du mot au num 5 et non a l'index 
+    print(chaine)
+    # I.3. Creation d'une nouvelle liste avec les elements de la liste precedente mais contenant la lettre "a"
+    print("\n-----3. Creation d'une nouvelle liste avec les elements de la liste precedente mais contenant la lettre a----\n")
+    n_chaine = []
+    #print(type(n_chaine))
+    for a in chaine:
+      if 'a' in a:
+        n_chaine.append(a)
+        
+    print("\nListe de mots contenant a sont:\n",n_chaine)
     
